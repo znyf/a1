5a82124cd302afac42ad9703985231a7e4f51a6b
hekun <1033699689@qq.com>
2020-07-29 17:01:42 +0800

    bbb

---
+++ src/main/java/club/znyf/test1.java
@@ -0,0 +1,11 @@
+package club.znyf;
+
+/**
+ * @author hk
+ * @data 2020/7/29 - 17:00
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("!@313123213");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+adasfsadf
+asdfsa
+</body>
+</html>
