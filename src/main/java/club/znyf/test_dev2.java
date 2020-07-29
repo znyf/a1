b3266771031a4233bffe7e104c32478b240d713b
hekun <1033699689@qq.com>
2020-07-29 17:58:48 +0800

    dev2 写的

---
+++ src/main/java/club/znyf/test_dev2.java
@@ -0,0 +1,11 @@
+package club.znyf;
+
+/**
+ * @author hk
+ * @data 2020/7/29 - 17:58
+ */
+public class test_dev2 {
+    public static void main(String[] args) {
+        System.out.println("dev2_test");
+    }
+}
