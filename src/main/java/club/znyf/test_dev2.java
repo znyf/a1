82ce67e71c88de0254305dd8db880818800724e4
hekun <1033699689@qq.com>
2020-07-29 18:00:07 +0800

    dev2 写的

--- src/main/java/club/znyf/test_dev2.java
+++ src/main/java/club/znyf/test_dev2.java
@@ -8,4 +8,7 @@ public class test_dev2 {
     public static void main(String[] args) {
         System.out.println("dev2_test");
     }
+    public void test(){
+        System.out.println("asdfasfasd");
+    }
 }
